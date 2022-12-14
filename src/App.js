63655e608aa6f32aa160327c7b42418c541e650a
Stephen Cefali <scefali@sentry.io>
2022-12-14 10:41:42 -0800

    adds sentry

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const methodDoesNotExist = {};
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => methodDoesNotExist()}>Break the world</button>
       </header>
     </div>
   );
